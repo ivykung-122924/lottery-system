681f55c949a7eba86d1c7fcdf99004ac97f2113c
ivy <ivylaya2013@gmail.com>
2026-04-30 09:37:33 +0800

    fix dotenv for deployment

--- server/index.js
+++ server/index.js
@@ -13,7 +13,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 dotenv.config();
-dotenv.config({ path: "server/.env" });
 
 const app = express();
 
